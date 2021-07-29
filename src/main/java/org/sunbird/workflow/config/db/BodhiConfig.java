d217b8354876c4746cdcb6fafc4797f162859ff1
karthik-tarento <karthikeyan.rajendran@tarento.com>
2021-07-29 11:42:49 +0530

    Added code to handle cassandra cluster IP

--- src/main/java/org/sunbird/workflow/config/db/BodhiConfig.java
+++ src/main/java/org/sunbird/workflow/config/db/BodhiConfig.java
@@ -1,8 +1,5 @@
 package org.sunbird.workflow.config.db;
 
-import com.datastax.driver.core.AuthProvider;
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.PlainTextAuthProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,9 +7,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
 import org.springframework.data.cassandra.config.CassandraSessionFactoryBean;
 import org.springframework.data.cassandra.core.CassandraAdminTemplate;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import com.datastax.driver.core.AuthProvider;
+import com.datastax.driver.core.PlainTextAuthProvider;
 
 @Configuration
 @ConfigurationProperties("spring.data.cassandra.bodhi")
@@ -40,9 +41,20 @@ public class BodhiConfig extends CassandraConfig {
 
 		AuthProvider authProvider = new PlainTextAuthProvider(bodhiUser, bodhiPassword);
 
+		CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
+		cluster.setContactPoints(getContactPoints());
+		cluster.setPort(getPort());
+		cluster.setAuthProvider(authProvider);
+		cluster.setJmxReportingEnabled(false);
+		try {
+			cluster.afterPropertiesSet();
+		} catch (Exception e) {
+			logger.error("Failed to construct Cassandra Cluster Object. ", e);
+			return null;
+		}
+		
 		CassandraSessionFactoryBean session = new CassandraSessionFactoryBean();
-		session.setCluster(Cluster.builder().addContactPoint(getContactPoints()).withPort(getPort())
-				.withAuthProvider(authProvider).withoutJMXReporting().build());
+		session.setCluster(cluster.getObject());
 		session.setConverter(cassandraConverter());
 		session.setKeyspaceName(getKeyspaceName());
 		session.setSchemaAction(getSchemaAction());
