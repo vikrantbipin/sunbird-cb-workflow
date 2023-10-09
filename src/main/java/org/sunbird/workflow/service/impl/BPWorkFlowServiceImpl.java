8070ec8a06d424e6c3dfb2cdaa2d378bf72d526a
saipradeep_ravipati <saipradeep.ravipati@tarento.com>
2023-10-09 14:23:13 +0530

    Removed filtering as Cassandra won't support the same

--- src/main/java/org/sunbird/workflow/service/impl/BPWorkFlowServiceImpl.java
+++ src/main/java/org/sunbird/workflow/service/impl/BPWorkFlowServiceImpl.java
@@ -232,7 +232,6 @@ public class BPWorkFlowServiceImpl implements BPWorkFlowService {
     private int getTotalApprovedUserCount(WfRequest wfRequest) {
         Map<String, Object> propertyMap = new HashMap<>();
         propertyMap.put(Constants.BATCH_ID, wfRequest.getApplicationId());
-        propertyMap.put(Constants.ACTIVE,Constants.TRUE);
         int totalCount = cassandraOperation.getCountByProperties(Constants.KEYSPACE_SUNBIRD_COURSES,
                 Constants.TABLE_ENROLMENT_BATCH_LOOKUP, propertyMap);
         return totalCount;
