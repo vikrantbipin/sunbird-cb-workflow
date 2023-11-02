12d432682ea299c1a9dbd7529ab2ae989c59bf75
Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
2023-11-02 14:12:48 +0530

    107258, 107261 changes merger (#127)
    Co-authored-by: Ravi Saurav <ravi.saurav@tarento.com>
    Co-authored-by: SaipradeepR <53404427+SaipradeepR@users.noreply.github.com>

--- src/main/java/org/sunbird/workflow/consumer/NotificationConsumer.java
+++ src/main/java/org/sunbird/workflow/consumer/NotificationConsumer.java
@@ -1,5 +1,7 @@
 package org.sunbird.workflow.consumer;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,10 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.sunbird.workflow.config.Constants;
+import org.sunbird.workflow.models.WfNotification;
 import org.sunbird.workflow.models.WfRequest;
 import org.sunbird.workflow.service.impl.NotificationServiceImpl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.sunbird.workflow.utils.CassandraOperation;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class NotificationConsumer {
@@ -22,30 +31,35 @@ public class NotificationConsumer {
 	@Autowired
 	private NotificationServiceImpl notificationService;
 
+	@Autowired
+	private CassandraOperation cassandraOperation;
+
 	@KafkaListener(groupId = "workflowNotificationTopic-consumer", topics = "${kafka.topics.workflow.notification}")
 	public void processMessage(ConsumerRecord<String, String> data) {
-		WfRequest wfRequest = null;
+		WfNotification wfNotification = null;
 		try {
 			String message = String.valueOf(data.value());
-			wfRequest = mapper.readValue(message, WfRequest.class);
-			logger.info("Recevied data in notification consumer : {}", mapper.writeValueAsString(wfRequest));
-			switch (wfRequest.getServiceName()) {
+			wfNotification = mapper.readValue(message, WfNotification.class);
+			Map<String, Object> courseAttributes = getCourseAttributes(wfNotification.getCourseId());
+			wfNotification.setCourseName((String) courseAttributes.get(Constants.COURSE_NAME));
+			logger.info("Recevied data in notification consumer : {}", mapper.writeValueAsString(wfNotification));
+			switch (wfNotification.getServiceName()) {
 				case Constants.PROFILE_SERVICE_NAME:
-					notificationService.sendNotification(wfRequest);
-					notificationService.sendNotificationToMdoAdmin(wfRequest);
+					notificationService.sendNotification(wfNotification);
+					notificationService.sendNotificationToMdoAdmin(wfNotification);
 					break;
 				case Constants.POSITION_SERVICE_NAME:
 				case Constants.DOMAIN_SERVICE_NAME:
 				case Constants.ORGANISATION_SERVICE_NAME:
-					notificationService.sendEmailNotification(wfRequest);
+					notificationService.sendEmailNotification(wfNotification);
 					break;
 				case Constants.BLENDED_PROGRAM_SERVICE_NAME:
 				case Constants.ONE_STEP_MDO_APPROVAL:
 				case Constants.ONE_STEP_PC_APPROVAL:
 				case Constants.TWO_STEP_MDO_AND_PC_APPROVAL:
 				case Constants.TWO_STEP_PC_AND_MDO_APPROVAL:
-					notificationService.sendNotification(wfRequest);
-					notificationService.sendNotificationToMdoAdminAndPC(wfRequest);
+					notificationService.sendNotification(wfNotification);
+					notificationService.sendNotificationToMdoAdminAndPC(wfNotification);
 					break;
 				case Constants.USER_REGISTRATION_SERVICE_NAME:
 					// nothing to do
@@ -58,4 +72,18 @@ public class NotificationConsumer {
 			logger.error("Error while deserialization the object value", ex);
 		}
 	}
+
+	public Map<String, Object> getCourseAttributes(String courseId){
+		Map<String, Object> propertiesMap = new HashMap<>();
+		Map<String, Object> courseDetails = new HashMap<>();
+		propertiesMap.put(Constants.IDENTIFIER, courseId);
+		List<Map<String, Object>> coursesDataList = cassandraOperation.getRecordsByProperties(Constants.DEV_HIERARCHY_STORE,
+				Constants.CONTENT_HIERARCHY,
+				propertiesMap,
+				Arrays.asList(Constants.IDENTIFIER, Constants.HIERARCHY));
+		Map<String, Object> hierarchy = new Gson().fromJson((String) coursesDataList.get(0).get("hierarchy"), new TypeToken<HashMap<String, Object>>(){}.getType());
+		courseDetails.put(Constants.COURSE_NAME, hierarchy.get(Constants.NAME));
+		return courseDetails;
+	}
+
 }
---
+++ src/main/java/org/sunbird/workflow/models/WfNotification.java
@@ -0,0 +1,36 @@
+package org.sunbird.workflow.models;
+
+import java.util.Date;
+
+public class WfNotification extends WfRequest{
+
+    private String courseName;
+
+    private String batchName;
+
+    private Date batchStartDate;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getBatchName() {
+        return batchName;
+    }
+
+    public void setBatchName(String batchName) {
+        this.batchName = batchName;
+    }
+
+    public Date getBatchStartDate() {
+        return batchStartDate;
+    }
+
+    public void setBatchStartDate(Date batchStartDate) {
+        this.batchStartDate = batchStartDate;
+    }
+}
