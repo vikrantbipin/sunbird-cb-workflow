f9b8ed40873792bf844ed5b2d74964d735db4fca
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-07-08 11:28:40 +0530

    4.8.27 dev v2 (#88)

--- src/main/java/org/sunbird/workflow/service/impl/WorkflowServiceImpl.java
+++ src/main/java/org/sunbird/workflow/service/impl/WorkflowServiceImpl.java
@@ -142,7 +142,7 @@ public class WorkflowServiceImpl implements Workflowservice {
 			wfIds.add(changeStatusResponse.get(Constants.WF_ID_CONSTANT));
 			changedStatus = changeStatusResponse.get(Constants.STATUS);
 		}
-		if (wfRequest.getServiceName().equalsIgnoreCase(Constants.PROFILE_SERVICE_NAME)) {
+		if (wfRequest.getServiceName().equalsIgnoreCase(Constants.PROFILE_SERVICE_NAME) && !wfRequest.getAction().equalsIgnoreCase(Constants.WITHDRAW)) {
 			sendNotification(wfRequest);
 		}
 		data.put(Constants.STATUS, changedStatus);
