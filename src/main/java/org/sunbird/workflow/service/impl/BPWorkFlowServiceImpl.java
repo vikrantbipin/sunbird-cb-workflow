28b7886add069bba30f02ea570993f3f876dd566
Rajeev Sathish <rajeev.sathish@tarento.com>
2025-11-18 12:37:28 +0530

    Revert "KB-11682 resolved the bug for nominate the user in BP"

--- src/main/java/org/sunbird/workflow/service/impl/BPWorkFlowServiceImpl.java
+++ src/main/java/org/sunbird/workflow/service/impl/BPWorkFlowServiceImpl.java
@@ -1699,7 +1699,7 @@ public class BPWorkFlowServiceImpl implements BPWorkFlowService {
         entity.setServiceName(wfRequest.getServiceName());
         entity.setDeptName(wfRequest.getDeptName());
         entity.setCurrentStatus(Constants.APPROVED);
-        entity.setInWorkflow(true);
+        entity.setInWorkflow(false);
         entity.setCreatedOn(new Date());
         entity.setLastUpdatedOn(new Date());
         entity.setRootOrg(rootOrg);
